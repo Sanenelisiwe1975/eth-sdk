a8d1140409fbb7fa76d8c03b933bbf6a9474d87c
sasimatjhb024 <sasimatjhb024@student.wethinkcode.co.za>
2025-09-25 12:38:36 +0200

    joson packeges

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const { ethers } = require("ethers");
+const { formatEther } = ethers;
+
+class MyEthereumSDK {
+  constructor(rpcUrl) {
+    this.provider = new ethers.JsonRpcProvider(rpcUrl);
+  }
+
+  // Get current block number
+  async getBlockNumber() {
+    return await this.provider.getBlockNumber();
+  }
+
+  // Get block details by number or hash
+  async getBlock(blockId) {
+    return await this.provider.getBlock(blockId);
+  }
+
+  // Get ETH balance of an address
+  async getBalance(address) {
+    const balance = await this.provider.getBalance(address);
+    return formatEther(balance);
+  }
+
+  // Send a signed transaction (optional, advanced)
+  async sendTransaction(signedTx) {
+    const txResponse = await this.provider.sendTransaction(signedTx);
+    return txResponse;
+  }
+}
+
+module.exports = MyEthereumSDK;
